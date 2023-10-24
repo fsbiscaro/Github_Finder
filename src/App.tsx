dfc4a1fac5d19a1fcde393461e6b5d095fe36922
fsbiscaro <fsbiscaro@hotmail.com>
2023-10-24 14:42:07 -0300

    att1

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className='App'>
-      <h1>Github </h1>
+      <h1>Github Finder</h1>
     </div>
   )
 }
